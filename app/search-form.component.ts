24db55ca4a52eb4e861edd3b8a0b4f749909d0dd
Quentin Delance <quentin.delance@gmail.com>
2016-10-02 21:56:52 +0200

    A few layout tweaks

--- app/search-form.component.ts
+++ app/search-form.component.ts
@@ -7,8 +7,7 @@ export class Search {
                 public departureDate: string,
                 public returnDate: string,
                 public cabinClass: number,
-                public numberPassengers: number,
-                public oneWay: boolean) {
+                public numberPassengers: number) {
     }
 }
 
@@ -24,7 +23,7 @@ export class SearchFormComponent {
         {id: 4, name: 'First'},
     ];
 
-    model:Search = new Search('CDG', 'JFK', '01/09/2016', '10/12/2016', 2, 3, true);
+    model:Search = new Search('CDG', 'JFK', '01/09/2016', null, 2, 3);
 
     get dump() {
         return JSON.stringify(this.model);
