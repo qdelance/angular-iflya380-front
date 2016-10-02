0285792305b7bd81f5aa3bb41f3b35e7e97aee5a
Quentin Delance <quentin.delance@gmail.com>
2016-10-02 21:48:11 +0200

    .gitignore missing

---
+++ app/search-form.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+
+export class Search {
+    constructor(public departure: string,
+                public arrival: string,
+                public departureDate: string,
+                public returnDate: string,
+                public cabinClass: number,
+                public nbPassenger: number,
+                public oneWay: boolean) {
+    }
+}
+
+@Component({
+    selector: 'search-form',
+    templateUrl: './app/search.component.html'
+})
+export class SearchComponent {
+    cabinClasses = [
+        {id: 1, name: 'Economy'},
+        {id: 2, name: 'Premium'},
+        {id: 3, name: 'Business'},
+        {id: 4, name: 'First'},
+    ];
+
+    model:Search = new Search('CDG', 'JFK', '01/09/2016', '10/12/2016', 2, 3, true);
+
+    get dump() {
+        return JSON.stringify(this.model);
+    }
+}
